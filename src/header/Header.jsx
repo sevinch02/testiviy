a9cdfac3084ba08881a4fd3175dedd0cb84e3995
sevinch02 <najmiddinova2021@mail.ru>
2022-11-02 15:35:27 +0500

    0

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,16 +1,13 @@
 
 import React from "react";
 import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom"; 
+
 
 const Header = () => {
-    const navigate = useNavigate();
-    const API = 'https://top-link-app.herokuapp.com/api/'
     return (
         <>
-          
-            <button onClick={() => navigate(`${API}/auth/login`)}>Kirish</button>
-          
+            <Link className='link' to='/sign-in'> <button>Kirish</button>
+            </Link>
             <Link className='link' to='/sign-out'>  <button>Ro'yxatdan o'tish</button>
             </Link>
         </>
